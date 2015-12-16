001ed5a910b0c9ddbfa69d8468b0c8a12fe820c2
Samuel Mengistu <sam321pbs@gmail.com>
2015-12-16 15:18:03 -0500

    Recognizes chapters and splits them

--- app/src/main/java/com/example/sammengistu/readtome/bookpages/ThePlanetMappersBook.java
+++ app/src/main/java/com/example/sammengistu/readtome/bookpages/ThePlanetMappersBook.java
@@ -31,10 +31,8 @@ public class ThePlanetMappersBook implements MakeAPage {
     private static final String TAG = "The Planet Mappers";
     public static List<PageOfBook> mPagesOfTheBook = new ArrayList<>();
     private final String SPACE = " ";
-    private int wordCount; // Keeps track of where in the book you are
     private int pageNumber = 0; // Keeps track of the page you are on
     private Book mThePlanetMappersBookEpubLib;
-    private StringBuilder mEntireBook;
 
     private Context mContext;
     private int mChapterTracker;
@@ -51,7 +49,6 @@ public class ThePlanetMappersBook implements MakeAPage {
     }
 
     private void makeStringBuilderOfEntireBook() {
-        mEntireBook = new StringBuilder();
         mChapterNames = new ArrayList<>();
 
         AssetManager assetManager = mContext.getAssets();
@@ -62,7 +59,7 @@ public class ThePlanetMappersBook implements MakeAPage {
 
             InputStream epubInputStream = assetManager
 
-                .open("dave_dawson_with_the_eighth.epub");
+                .open("the_planet_mappers.epub");
 
             // Load Book from inputStream
 
@@ -77,6 +74,16 @@ public class ThePlanetMappersBook implements MakeAPage {
 
             mChapterTracker = 0;
 
+            StringBuilder chapterLabel = new StringBuilder();
+            StringBuilder page = new StringBuilder();
+            ArrayList<String> leftOverWordsFromPrevPage = new ArrayList<>();
+            int wordCount = 0;
+            boolean pageIsReady = false;
+            boolean haveChapterLabel = false;
+            int createFirstChapter = 0;
+            boolean skipNextLineChapterRemainder = false;
+            StringBuilder actualChapterLabelToCheckAgainst = new StringBuilder();
+
             for (SpineReference bookSection : spine.getSpineReferences()) {
 
                 Log.i("Section", "-----------------------");
@@ -88,15 +95,6 @@ public class ThePlanetMappersBook implements MakeAPage {
                     BufferedReader r = new BufferedReader(new InputStreamReader(is));
                     String line;
 
-                    StringBuilder chapterLabel = new StringBuilder();
-                    StringBuilder page = new StringBuilder();
-                    ArrayList<String> leftOverWordsFromPrevPage = new ArrayList<>();
-                    int wordCount = 0;
-                    boolean pageIsReady = false;
-                    boolean haveChapterLabel = false;
-                    int createFirstChapter = 0;
-
-
                     int MAX_NUMBER_OF_WORDS_PER_PAGE = 184;
 
                     while ((line = r.readLine()) != null) {
@@ -106,11 +104,27 @@ public class ThePlanetMappersBook implements MakeAPage {
                         line = line.replace("\n", "").replace("\r", "");
                         Log.i("Read it ", line);
 
+                        String [] lineIntoArray = line.split("\\s+");
+
+                        //Removes
+                        if (skipNextLineChapterRemainder){
+                            skipNextLineChapterRemainder = false;
+
+                            String [] chapterLabelArray = actualChapterLabelToCheckAgainst
+                                .toString().split("\\s+");
+
+                            //May not a
+//                            if (line.contains(chapterLabelArray[chapterLabelArray.length - 1])){
+//                                Log.i("Next", "Line = " + line + "ChapterLabelArray = " + chapterLabelArray[chapterLabelArray.length -1]);
+//                                continue;
+//                            }
+
+                        }
 
                         //Check if the current line is a chapter and decide if it needs a label
                         if (isItAChapterLabel(line)){
                             chapterLabel = new StringBuilder();
-                            chapterLabel.append(line);
+                            chapterLabel.append(mChapterNames.get(mChapterTracker - 1));
                             haveChapterLabel = true;
                             Log.i(TAG, "Found a chapter label : " + line);
 
@@ -120,18 +134,16 @@ public class ThePlanetMappersBook implements MakeAPage {
                                 mPagesOfTheBook.add(newPage);
                                 page = new StringBuilder();
                                 pageIsReady = false;
+                                wordCount = 0;
                             }
                             createFirstChapter++;
 
+                            skipNextLineChapterRemainder = true;
                             continue;
                         }
 
 
 
-
-
-
-                        String [] lineIntoArray = line.split("\\s+");
 
                         //Checks if the previous line had any leftover words before page break
                         if (!leftOverWordsFromPrevPage.isEmpty()){
@@ -188,91 +200,6 @@ public class ThePlanetMappersBook implements MakeAPage {
                             }
                         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//                        boolean isNextLineChapter = false;
-//                        //Checks if the next line is a new chapter
-//                        String followingLine = "";
-//                        if ((followingLine = r.readLine()) != null){
-//                            if (isItAChapterLabel(followingLine)){
-//                                isNextLineChapter = true;
-//                                Log.i(TAG, "Next Line is chapter");
-//                            }
-//                        }
-//
-//                        if (isItAChapterLabel(line)) {
-//
-//                            Log.i(TAG, "Found chapter match = " + line);
-//                            chapterLabel = new StringBuilder();
-//                            chapterLabel.append(line);
-//                            haveChapterLabel = true;
-//
-//                            continue;
-//                        } else {
-//                            String[] wordsInLineArray = line.split("\\s+");
-//
-//                            Log.i(TAG, "Words in array are " + line);
-//
-//                            for (int i = 0; i < wordsInLineArray.length; i++) {
-//
-//                                if (wordCount < MAX_NUMBER_OF_WORDS_PER_PAGE) {
-//                                    wordCount = 0;
-//                                    pageIsReady = true;
-//                                    break;
-//                                }
-//
-//                                if (isNextLineChapter){
-//                                    wordCount = 0;
-//                                    pageIsReady = true;
-//                                    break;
-//                                }
-//
-//                                page.append(wordsInLineArray[i] + SPACE);
-//                                wordCount++;
-//                            }
-//                        }
-//
-//                        if (pageIsReady) {
-//
-//                            if (haveChapterLabel) {
-//
-//                                PageOfBook newPage = new PageOfBook(
-//                                    page.toString(),
-//                                    pageNumber++,
-//                                    chapterLabel.toString());
-//
-//                                mPagesOfTheBook.add(newPage);
-//
-//                                haveChapterLabel = false;
-//
-//                            } else {
-//                                PageOfBook newPage = new PageOfBook(
-//                                    page.toString(),
-//                                    pageNumber++);
-//                                mPagesOfTheBook.add(newPage);
-//                            }
-//                            pageIsReady = false;
-//                            page = new StringBuilder();
-//                            chapterLabel = new StringBuilder();
-//                        }
                     }
                 } catch (IOException e) {
                 }
@@ -284,9 +211,6 @@ public class ThePlanetMappersBook implements MakeAPage {
         }
     }
 
-    private void createPage(){
-
-    }
 
     private boolean isItAChapterLabel(String line) {
         String firstWordInChapterName = "";
@@ -326,106 +250,6 @@ public class ThePlanetMappersBook implements MakeAPage {
     private void addPages() {
 
         makeStringBuilderOfEntireBook();
-
-//        for (String chapter : mChapterNames) {
-//            Log.i(TAG, chapter);
-//        }
-
-//        //creates an array of all the words from the book
-//        String[] allWordsFromBook = mEntireBook.toString().split("\\s+");
-//
-//        for (String word : allWordsFromBook) {
-//            if (mChapterNames.get(0).equals(word)) {
-//                Log.i("Word", word);
-//                break;
-//            }
-//        }
-//
-//        boolean makeTitlePageFirst = true;
-//        boolean makePageTwo = false;
-//
-//        for (wordCount = 0; wordCount < allWordsFromBook.length; wordCount++) {
-//
-//            if (makeTitlePageFirst) {
-//
-//                int TITLE_PAGE_LIMIT = 9;
-//                makeSpecialPage(allWordsFromBook, TITLE_PAGE_LIMIT);
-//
-//                makeTitlePageFirst = false;
-//                makePageTwo = true;
-//
-//            } else if (makePageTwo) {
-//                int LENGTH_OF_QUOTE = 33;
-//                makeSpecialPage(allWordsFromBook, LENGTH_OF_QUOTE);
-//                makePageTwo = false;
-//            } else {
-//
-//                StringBuilder chapterLabel = new StringBuilder();
-//                StringBuilder page = new StringBuilder();
-//                boolean hasChapter;
-//
-//                //Builds a chapter label
-//                if (allWordsFromBook[wordCount].equals(mChapterNames.get(0).replaceAll("\\s+", ""))) {
-//                    Log.i(TAG, "found chapter 1");
-//                    wordCount++;
-//
-//                    Log.i(TAG, allWordsFromBook[wordCount - 1]);
-//
-//                    hasChapter = true;
-//
-//                    for (int i = 0; wordCount < allWordsFromBook.length; wordCount++) {
-//
-//                        if (allWordsFromBook[wordCount].equals("+/")) {
-//                            Log.i(TAG, chapterLabel.toString());
-//                            wordCount++;
-//                            Log.i(TAG, allWordsFromBook[wordCount - 1]);
-//                            break;
-//                        } else {
-//                            chapterLabel.append(allWordsFromBook[wordCount]).append(" ");
-//                        }
-//                    }
-//
-//                } else {
-//                    hasChapter = false;
-//                }
-//
-//                //Puts together enough words to fill a page
-//                int MAX_NUMBER_OF_WORDS_PER_PAGE = 184;
-//                for (int goToMaxNumberOfWordsPerPage = 0;
-//                     goToMaxNumberOfWordsPerPage < MAX_NUMBER_OF_WORDS_PER_PAGE;
-//                     goToMaxNumberOfWordsPerPage++) {
-//
-//                    if (wordCount == allWordsFromBook.length) {
-//                        break;
-//                    }
-//                    if (allWordsFromBook[wordCount].equals("/+")) {
-//                        break;
-//                    } else {
-//                        page.append(allWordsFromBook[wordCount++]).append(SPACE);
-//                    }
-//                }
-//
-//                if (hasChapter) {
-//
-//                    PageOfBook newPage = new PageOfBook(
-//                        page.toString(),
-//                        pageNumber++,
-//                        chapterLabel.toString());
-//
-//                    mPagesOfTheBook.add(newPage);
-//                    wordCount--;
-//
-//                } else {
-//                    PageOfBook newPage = new PageOfBook(
-//                        page.toString(),
-//                        pageNumber++);
-//                    mPagesOfTheBook.add(newPage);
-//                    wordCount--;
-//                }
-//            }
-//        }
-
-//        for (String)
 
     }
 
@@ -488,28 +312,5 @@ public class ThePlanetMappersBook implements MakeAPage {
 
         }
 
-    }
-
-    /**
-     * Used to build the title page and the quote page
-     *
-     * @param allWordsFromBook - gets words from the book
-     * @param length           - number of words from that page
-     */
-    private void makeSpecialPage(String[] allWordsFromBook, int length) {
-
-        StringBuilder page = new StringBuilder();
-
-        //adds title
-        for (int temp = 0; temp < length; temp++) {
-
-            page.append(allWordsFromBook[wordCount++]).append(SPACE);
-        }
-
-        wordCount--;
-        PageOfBook newPage = new PageOfBook(
-            page.toString(),
-            pageNumber++);
-        mPagesOfTheBook.add(newPage);
     }
 }
